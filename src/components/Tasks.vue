bf6b92512699d8b3cc83e026872db8e50840110b
designeng <denissavenok@gmail.com>
2021-03-01 20:37:55 +0300

    Import dynamicly /data/data.json in created hook

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -30,7 +30,7 @@ export default {
     data () {
         return {
             isLoading: true,
-            perPage: 3,
+            perPage: 10,
             currentPage: 1,
             items: []
         }
@@ -40,21 +40,11 @@ export default {
             return this.items.length
         }
     },
-    created () {
-        setTimeout(() => {
-            this.items = [
-                { id: 1, first_name: 'Fred', last_name: 'Flintstone' },
-                { id: 2, first_name: 'Wilma', last_name: 'Flintstone' },
-                { id: 3, first_name: 'Barney', last_name: 'Rubble' },
-                { id: 4, first_name: 'Betty', last_name: 'Rubble' },
-                { id: 5, first_name: 'Pebbles', last_name: 'Flintstone' },
-                { id: 6, first_name: 'Bamm Bamm', last_name: 'Rubble' },
-                { id: 7, first_name: 'The Great', last_name: 'Gazzoo' },
-                { id: 8, first_name: 'Rockhead', last_name: 'Slate' },
-                { id: 9, first_name: 'Pearl', last_name: 'Slaghoople' }
-            ];
-            this.isLoading = false;
-        }, 1000)
+    async created () {
+        const data = await import('../../data/data.json');
+
+        this.items = data.default;
+        this.isLoading = false;
     }
 }
 </script>
