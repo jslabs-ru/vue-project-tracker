bd8b2f05a294472ed47edce0aa118bf7adab1e9e
designeng <denissavenok@gmail.com>
2021-03-02 19:22:43 +0300

    Install lodash. Add components/paginator & styles

---
+++ src/components/paginator/Paginator.vue
@@ -0,0 +1,44 @@
+<template>
+    <ul class="paginator" v-if="pages.length > 1">
+        <paginator-item
+            v-for="page in pages"
+            :page="page"
+            :activePage="activePage"
+            :key="page"
+            @page-click="onPageClick"
+        ></paginator-item>
+    </ul>
+</template>
+
+<script>
+import PaginatorItem from '@/components/paginator/PaginatorItem.vue';
+
+export default {
+    name: 'Paginator',
+    components: {
+        PaginatorItem
+    },
+    data () {
+        return {
+            activePage: 1
+        }
+    },
+    props: {
+        pagesCount: {type: Number}
+    },
+    computed: {
+        pages () {
+            let pages = [];
+            for (var i = 1; i <= this.pagesCount; i++) {
+                pages.push(i);
+            }
+            return pages;
+        },
+    },
+    methods: {
+        onPageClick (page) {
+            this.$emit('page-click', page);
+        }
+    }
+}
+</script>
---
+++ src/components/paginator/PaginatorItem.vue
@@ -0,0 +1,27 @@
+<template>
+    <a
+        class="paginator-item"
+        v-bind:class="{'paginator-item--active': isActive}"
+        @click="$emit('page-click', page)"
+    >&nbsp;{{ page }}&nbsp;</a>
+</template>
+
+<script>
+export default {
+    name: 'PaginatorItem',
+    props: {
+        page: {type: Number},
+        activePage: {type: Number},
+    },
+    computed: {
+        isActive () {
+            return this.activePage === this.page;
+        },
+    },
+    methods: {
+        onPageClick (page) {
+
+        }
+    }
+}
+</script>
