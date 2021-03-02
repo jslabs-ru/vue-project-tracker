983555159ef05290204211a69bdcc09beccca253
designeng <denissavenok@gmail.com>
2021-03-02 13:31:45 +0300

    Fix: add src/services

---
+++ src/services/userService.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+
+const USERS_ENDPOINT = '/api/v1/users';
+
+const UserService = {
+    getAll () {
+        return axios({
+            url: USERS_ENDPOINT
+        }).then(res => {
+            return res.data;
+        }).catch(error => {
+            throw new Error('[UserService getAll]' + error.message)
+        })
+    },
+
+    getUserAccountData (userid) {
+        console.log('userid.....', userid);
+        return axios({
+            url: `${USERS_ENDPOINT}/${userid}`
+        }).then(res => {
+            console.log('res...', res);
+            return res.data;
+        }).catch(error => {
+            throw new Error('[UserService getUserAccountData]' + error.message)
+        })
+    }
+}
+
+export default UserService;
