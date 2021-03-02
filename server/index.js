feb9cba4891978f70a154c76c446aa36755dc6bd
designeng <denissavenok@gmail.com>
2021-03-02 15:34:19 +0300

    Fix api version

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 
 const PORT = parseInt(process.env.PORT) || 5000;
 
-app.use('/api/v1', createApiRouter(app));
+app.use('/api/v2', createApiRouter(app));
 
 async function startServer(db) {
     app.set('db', db);
