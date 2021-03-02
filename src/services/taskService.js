c647f67948ec766f5cc051d3c212cd92f95eab8b
designeng <denissavenok@gmail.com>
2021-03-02 19:23:27 +0300

    Add getTasksCount method & payload in getAll

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -3,15 +3,31 @@ import axios from 'axios';
 const TASKS_ENDPOINT = '/api/v2/tasks';
 
 const TaskService = {
-    getAll () {
+    getAll (payload) {
+        const { from, to } = payload;
+        
+        let url = from && to
+            ?  `${TASKS_ENDPOINT}?from=${from}&to=${to}`
+            : TASKS_ENDPOINT;
+
         return axios({
-            url: TASKS_ENDPOINT
+            url
         }).then(res => {
             return res.data;
         }).catch(error => {
             throw new Error('[TaskService getAll]' + error.message)
         })
-    }
+    },
+
+    getTasksCount () {
+        return axios({
+            url: TASKS_ENDPOINT + '?count=1'
+        }).then(res => {
+            return res.data;
+        }).catch(error => {
+            throw new Error('[getTasksCount getAll]' + error.message)
+        })
+    },
 }
 
 export default TaskService;
