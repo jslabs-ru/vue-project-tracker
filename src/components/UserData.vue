8baea7be898c1c8645964078ab4d44e924fda8c6
designeng <denissavenok@gmail.com>
2021-03-06 02:16:44 +0300

    Use tabs & countdown message with redirect after delete

--- src/components/UserData.vue
+++ src/components/UserData.vue
@@ -6,31 +6,48 @@
             label="Spinning"
         ></b-spinner>
 
-        <b-card
-            v-if="user"
-            :title="user.name"
-            style="max-width: 20rem;"
-            class="mb-2"
-        >
-            <b-card-text>
-                {{ user.username }}
-            </b-card-text>
+        <b-tabs content-class="mt-3" v-if="user">
+            <b-tab title="User" >
+                <b-card
+                    v-if="user"
+                    :title="user.name"
+                    style="max-width: 20rem;"
+                    class="mb-2"
+                >
+                    <b-card-text>
+                        {{ user.username }}
+                    </b-card-text>
 
-            <b-card-text>
-                {{ user.email }}
-            </b-card-text>
-        </b-card>
+                    <b-card-text>
+                        {{ user.email }}
+                    </b-card-text>
+                </b-card>
+            </b-tab>
+            <b-tab
+                title="Dangerous"
+            >
+                <button
+                    v-if="!isDeleted"
+                    type="submit"
+                    class="btn btn-danger"
+                    v-on:click="onDelete"
+                >Delete this user</button>
+            </b-tab>
+        </b-tabs>
     </div>
 </template>
 
 <script>
 import UserService from '@/services/userService';
 
+const CONFIRMATION_DELETE_MESSAGE = 'Delete this user?';
+
 export default {
     name: 'UserData',
     data () {
         return {
             isLoading: true,
+            isDeleted: false,
             user: null
         }
     },
@@ -56,6 +73,32 @@ export default {
             .finally(() => {
                 this.isLoading = false;
             })
+    },
+    methods: {
+        getSuccessMessage (timer) {
+            return `User deleted. You are going to be redirected to users list in ${timer} seconds`;
+        },
+        onDelete () {
+            if (window.confirm(CONFIRMATION_DELETE_MESSAGE)) {
+                UserService.deleteUserAccount(this.userid)
+                    .then(res => {
+                        let timer = 5;
+                        this.isDeleted = true;
+                        let vm = this;
+
+                        let countDownInterval = setInterval(function traceMessage() {
+                            if(timer === 0) {
+                                clearInterval(countDownInterval)
+                                vm.$router.push({path: '/users'}, () => {})
+                            } else {
+                                vm.$success(vm.getSuccessMessage(timer));
+                                timer--;
+                            }
+                            return traceMessage;
+                        }(), 1000);
+                    })
+            }
+        }
     }
 }
 </script>
