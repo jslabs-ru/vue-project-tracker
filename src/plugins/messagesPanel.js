fe2fb72560bb1a6df077ffae15042f405d26d869
designeng <denissavenok@gmail.com>
2021-03-06 02:15:42 +0300

    Add Vue.prototype.$success method - show .alert-success

--- src/plugins/messagesPanel.js
+++ src/plugins/messagesPanel.js
@@ -1,11 +1,23 @@
-const alertClasses = ['alert', 'alert-danger'];
-
 export default {
     install (Vue) {
         Vue.prototype.$error = function (message) {
-            let alertElement = this.$el.querySelector('.alert');
+            let alertElement = this.$el.querySelector('.alert-danger');
             if(!alertElement) {
                 alertElement = document.createElement('div');
+                const alertClasses = ['alert', 'alert-danger'];
+                alertElement.classList.add(...alertClasses);
+                alertElement.textContent = message;
+                this.$el.appendChild(alertElement);
+            } else {
+                alertElement.textContent = message;
+            }
+        }
+
+        Vue.prototype.$success = function (message) {
+            let alertElement = this.$el.querySelector('.alert-success');
+            if(!alertElement) {
+                alertElement = document.createElement('div');
+                const alertClasses = ['alert', 'alert-success'];
                 alertElement.classList.add(...alertClasses);
                 alertElement.textContent = message;
                 this.$el.appendChild(alertElement);
