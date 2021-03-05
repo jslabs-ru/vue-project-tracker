3526fa79e01cd7313b8739a356afc9ef8f3bd6f9
designeng <denissavenok@gmail.com>
2021-03-05 17:35:19 +0300

    Add Vue.prototype.$error logic

--- src/plugins/messagesPanel.js
+++ src/plugins/messagesPanel.js
@@ -1,7 +1,17 @@
+const alertClasses = ['alert', 'alert-danger'];
+
 export default {
     install (Vue) {
         Vue.prototype.$error = function (message) {
-
+            let alertElement = this.$el.querySelector('.alert');
+            if(!alertElement) {
+                alertElement = document.createElement('div');
+                alertElement.classList.add(...alertClasses);
+                alertElement.textContent = message;
+                this.$el.appendChild(alertElement);
+            } else {
+                alertElement.textContent = message;
+            }
         }
     }
 }
