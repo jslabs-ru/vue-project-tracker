713e9d87d44d6a1d18b8de44c5360bce764abd5b
designeng <denissavenok@gmail.com>
2021-03-09 12:11:36 +0300

    Extract Autocomplete in separate component for re-use. Provide serviceMethod & resultToListMappingMethod in props

--- src/components/TaskAutocomplete.vue
+++ src/components/TaskAutocomplete.vue
@@ -1,129 +1,59 @@
 <template>
-    <div class="autocomplete-root">
-        <input ref="autocomplete-input"
-            v-focus
-            id="autocomplete-input"
-            type="text"
-            class="form-control"
-            autocomplete="off"
-            v-model="autocompleteInputModel"
-            @input="debouncedApiRequest"
-            @keyup.up="onUp"
-            @keyup.down="onDown"
-            @keyup.enter="onEnter"
-        />
+    <div class="row">
+        <div class="cell">
+            <label for="task-autocomplete">Choose task</label>
+            <Autocomplete
+                id="task-autocomplete"
+                :serviceMethod="serviceMethod"
+                :resultToListMappingMethod="resultToListMappingMethod"
+                @autocomplete-selected="onTaskSelected"
+                @autocomplete-not-selected="onTaskNotSelect"
+            />
+        </div>
 
-        <div class="autocomplete-container" style="width: 18rem;"
-            v-if="selected === false"
-        >
-            <ul class="autocomplete-list">
-                <li class="autocomplete-list-item"
-                    v-for="(item, index) in autocompleteList"
-                    :key="index"
-                    :class="{'active-autocomplete-list-item': positionInList === index}"
-                    @click="onSelect(item)"
-                ><span>{{ item }}</span></li>
-            </ul>
+        <div class="cell">
+            <div v-if="task" class="task-found">Found: <span class="green">{{ task }}</span></div>
         </div>
     </div>
 </template>
 
 <script>
-import { map, debounce } from 'lodash';
 
+import Autocomplete from '@/components/autocomplete/Autocomplete';
 import TaskService from '@/services/taskService';
+
+import { map } from 'lodash';
 
 export default {
     name: 'TaskAutocomplete',
+    components: {
+        Autocomplete
+    },
     data () {
         return {
-            selected: false,
-            positionInList: 0,
-            autocompleteList: [],
-            autocompleteInputModel: ''
+            task: null,
+            serviceMethod: TaskService.getTasksByToken,
+            resultToListMappingMethod: function(res) {
+                return map(res, item => item.description);
+            }
         }
     },
-    created () {
-        this.debouncedApiRequest = debounce(this.apiRequest, 100);
-    },
     methods: {
-        apiRequest (event) {
-            this.selected = false;
-            const token = event.target.value;
-            return TaskService.getTasksByToken(token)
-                .then(res => {
-                    if(res.length) {
-                        this.autocompleteList = map(res, item => item.description);
-                    } else {
-                        this.autocompleteList = [];
-                    }
-                })
+        onTaskSelected (task) {
+            this.task = task;
         },
-        onSelect (item) {
-            this.autocompleteInputModel = item;
-            this.$emit('task-selected', item);
-            this.selected = true;
-            this.autocompleteList = [];
-        },
-        onUp () {
-            this.positionInList--;
-            if(this.positionInList === -1) this.positionInList = this.autocompleteList.length - 1;
-        },
-        onDown () {
-            this.positionInList++;
-            if(this.positionInList === this.autocompleteList.length) this.positionInList = 0;
-        },
-        onEnter () {
-            this.onSelect(this.autocompleteList[this.positionInList]);
-            this.positionInList = 0;
+        onTaskNotSelect () {
+            this.task = null;
         }
     }
 }
 </script>
 
 <style>
-.autocomplete-root {
-    position: relative;
+.green {
+    color: green;
 }
-
-.autocomplete-done {
-    cursor: pointer;
-}
-
-#autocomplete-input {
-    width: 300px;
-}
-
-.autocomplete-container {
-    position: absolute;
-    min-height: 182px;
-    min-width: 100px;
-    top: 42px;
-    left: 0px;
-    z-index: 100;
-    background-color: #fff;
-}
-
-ul.autocomplete-list {
-    list-style: none;
-    width: 100%;
-    height: 100%;
-    background-color: #F9E9D5;
-    opacity: 0.7;
-    font-size: 12px;
-}
-
-.autocomplete-list-item {
-    width: 100%;
-    background-color: #F9E9D5;
-    cursor: pointer;
-}
-.autocomplete-list-item span {
-    margin-left: 9px;
-}
-.active-autocomplete-list-item {
-    background-color: #FCDCB5;
-    width: 100%;
-    height: 18px;
+.task-found {
+    margin: 30px;
 }
 </style>
---
+++ src/components/autocomplete/Autocomplete.vue
@@ -0,0 +1,139 @@
+<template>
+    <div class="autocomplete-root">
+        <input ref="autocomplete-input"
+            v-focus
+            id="autocomplete-input"
+            type="text"
+            class="form-control"
+            autocomplete="off"
+            v-model="autocompleteInputModel"
+            @input="debouncedApiRequest"
+            @keyup.up="onUp"
+            @keyup.down="onDown"
+            @keyup.enter="onEnter"
+        />
+
+        <div class="autocomplete-container" style="width: 18rem;"
+            v-if="selected === false"
+        >
+            <ul class="autocomplete-list">
+                <li class="autocomplete-list-item"
+                    v-for="(item, index) in autocompleteList"
+                    :key="index"
+                    :class="{'active-autocomplete-list-item': positionInList === index}"
+                    @click="onSelect(item)"
+                ><span>{{ item }}</span></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import { debounce } from 'lodash';
+
+export default {
+    name: 'Autocomplete',
+    data () {
+        return {
+            selected: false,
+            positionInList: 0,
+            autocompleteList: [],
+            autocompleteInputModel: ''
+        }
+    },
+    props: {
+        serviceMethod: {
+            type: Function,
+            required: true
+        },
+        resultToListMappingMethod: {
+            type: Function,
+            required: true
+        }
+    },
+    created () {
+        this.debouncedApiRequest = debounce(this.apiRequest, 100);
+    },
+    methods: {
+        apiRequest (event) {
+            this.selected = false;
+            this.$emit('autocomplete-not-selected');
+
+            const token = event.target.value;
+            return this.serviceMethod(token)
+                .then(res => {
+                    if(res.length) {
+                        this.autocompleteList = this.resultToListMappingMethod(res);
+                    } else {
+                        this.autocompleteList = [];
+                    }
+                })
+        },
+        onSelect (item) {
+            this.autocompleteInputModel = item;
+            this.$emit('autocomplete-selected', item);
+            this.selected = true;
+            this.autocompleteList = [];
+        },
+        onUp () {
+            this.positionInList--;
+            if(this.positionInList === -1) this.positionInList = this.autocompleteList.length - 1;
+        },
+        onDown () {
+            this.positionInList++;
+            if(this.positionInList === this.autocompleteList.length) this.positionInList = 0;
+        },
+        onEnter () {
+            this.onSelect(this.autocompleteList[this.positionInList]);
+            this.positionInList = 0;
+        }
+    }
+}
+</script>
+
+<style>
+.autocomplete-root {
+    position: relative;
+}
+
+.autocomplete-done {
+    cursor: pointer;
+}
+
+#autocomplete-input {
+    width: 300px;
+}
+
+.autocomplete-container {
+    position: absolute;
+    min-height: 182px;
+    min-width: 100px;
+    top: 42px;
+    left: 0px;
+    z-index: 100;
+    background-color: #fff;
+}
+
+ul.autocomplete-list {
+    list-style: none;
+    width: 100%;
+    height: 100%;
+    background-color: #F9E9D5;
+    opacity: 0.7;
+    font-size: 12px;
+}
+
+.autocomplete-list-item {
+    width: 100%;
+    background-color: #F9E9D5;
+    cursor: pointer;
+}
+.autocomplete-list-item span {
+    margin-left: 9px;
+}
+.active-autocomplete-list-item {
+    background-color: #FCDCB5;
+    width: 100%;
+    height: 18px;
+}
+</style>
