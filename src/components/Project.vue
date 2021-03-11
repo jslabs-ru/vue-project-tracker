334d45fb42e5a78b19aa24a388c5712a20b44269
designeng <denissavenok@gmail.com>
2021-03-11 14:48:52 +0300

    Prettify

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -1,7 +1,32 @@
 <template>
-    <pre>
-        {{ project }}
-    </pre>
+    <div>
+        <b-spinner
+            v-if="isLoading"
+            variant="primary"
+            label="Spinning"
+        ></b-spinner>
+
+        <b-card
+            v-else
+            style="max-width: 20rem;"
+            class="mb-2"
+        >
+            <template #header>
+                <b-card-title>project.name</b-card-title>
+            </template>
+
+            <b-card-body>
+                <b-card-text>
+                    {{ project.description }}
+                </b-card-text>
+            </b-card-body>
+
+            <template #footer>
+                <div class="sub-title"><span class="sub-title-label">Created at:</span> {{ project.created_at }}</div>
+                <div class="sub-title"><span class="sub-title-label">Updated at:</span> {{ project.updated_at }}</div>
+            </template>
+        </b-card>
+    </div>
 </template>
 
 <script>
@@ -13,13 +38,25 @@ export default {
     name: 'Project',
     data () {
         return {
+            isLoading: true,
             project: null
         }
     },
     created () {
         ProjectService.getProjectById(this.$route.params.id).then(project => {
+            this.isLoading = false;
             this.project = project;
         })
     }
 }
 </script>
+
+<style>
+.sub-title {
+    font-size: 12px;
+    font-style: italic;
+}
+.sub-title-label {
+    font-weight: bold;
+}
+</style>
