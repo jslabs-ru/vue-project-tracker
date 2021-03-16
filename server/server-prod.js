5ac8c1f50c494d4fb4ce9144f89b9344619fbada
designeng <denissavenok@gmail.com>
2021-03-16 19:36:18 +0300

    Install connect-history-api-fallback. Add server/server-prod.js. Add start command: run app in prod mode on 5000 port with connect-history-api-fallback

---
+++ server/server-prod.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const knex = require('knex');
+const express = require('express');
+const history = require('connect-history-api-fallback');
+
+const createApiRouter = require('./apiRouter');
+
+const app = express();
+const staticFileMiddleware = express.static(__dirname + '/../dist');
+
+const PORT = parseInt(process.env.PORT) || 5000;
+
+const historyMiddleware = history({
+    disableDotRule: true,
+    verbose: false
+});
+
+app.use(staticFileMiddleware);
+
+app.use((req, res, next) => {
+    if (req.path.match(/^\/api\/v2\//)) {
+        next();
+    } else {
+        historyMiddleware(req, res, next);
+    }
+});
+
+app.use(staticFileMiddleware);
+
+app.use('/api/v2', createApiRouter(app));
+
+async function startServer(db) {
+    app.set('db', db);
+
+    const server = app.listen(PORT, function() {
+        console.log('Server is running: http://localhost:%j', PORT);
+    });
+
+    process.on('message', (message) => {
+        if (message === 'shutdown') {
+            console.log('Stop express server');
+            server.close(() => {
+                process.exit();
+            });
+        }
+    });
+}
+
+startServer(knex({
+    client: 'sqlite3',
+    connection: {
+        filename: './data/data.db',
+    },
+    useNullAsDefault: true
+}));
