d1534932a25879a4687a07649d21b40abb741b81
designeng <denissavenok@gmail.com>
2021-03-18 16:01:42 +0300

    Cleanup

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,11 @@
 <template>
     <div class="root-page">
         Project Tracker - Main page
-
-        <!-- <button
-            type="submit"
-            class="btn btn-primary"
-            @click="createProject"
-        >Create project</button> -->
     </div>
 </template>
 
 <script>
 export default {
-    name: 'Home',
-    methods: {
-        // createProject () {
-        //     this.$router.push({path: '/projects/1'}, () => {})
-        // }
-    }
+    name: 'Main'
 }
 </script>
