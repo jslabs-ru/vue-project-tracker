c7a1e9b331ae8852f93652e35f308759d8064f8c
designeng <denissavenok@gmail.com>
2021-03-18 16:21:57 +0300

    Add Mode example page, calculate mode prop

---
+++ src/components/Mode.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="root-page">
+        Current mode: {{ mode }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Mode',
+    props: {
+        mode: String
+    }
+}
+</script>
--- src/router/base.js
+++ src/router/base.js
@@ -1,14 +1,28 @@
 import Main from '@/components/Main.vue';
+import Mode from '@/components/Mode.vue';
 import NotFound from '@/components/NotFound.vue';
 
 export default [
     {
         path: '/',
-        components: {
-            default: Main
-        },
+        component: Main,
         meta: {
             title: 'Main page'
+        }
+    },
+    {
+        path: '/mode',
+        component: Mode,
+        props: route => {
+            const { mode } = route.query;
+            if(mode === 'dev') {
+                return { mode: 'DEVELOPMENT' }
+            } else {
+                return { mode: 'PRODUCTION' }
+            }
+        },
+        meta: {
+            title: 'Mode example page'
         }
     },
     {
