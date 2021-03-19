b97057779094d6df975700488c4daaa6d0352fd9
designeng <denissavenok@gmail.com>
2021-03-19 15:32:41 +0300

    Inject BASE_PAGE_TITLE

--- src/router/hooks/beforeEach.js
+++ src/router/hooks/beforeEach.js
@@ -1,7 +1,6 @@
-const BASE_TITLE = 'Project Tracker - ';
-
 export default function beforeEach(to, from, next) {
     const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title);
-    if(nearestWithTitle) document.title = BASE_TITLE + nearestWithTitle.meta.title;
+    if(nearestWithTitle) document.title =
+        /* defined in vue.config.js, injected with DefinePlugin */ BASE_PAGE_TITLE + ' - ' + nearestWithTitle.meta.title;
     next();
 }
--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
+const BASE_PAGE_TITLE = 'Project Tracker';
+
 module.exports = {
+    chainWebpack: config => {
+        config.plugin('define').tap(definitions => {
+            definitions[0] = Object.assign(definitions[0], {
+                BASE_PAGE_TITLE: JSON.stringify(BASE_PAGE_TITLE)
+            })
+            return definitions;
+        })
+    },
     pages: {
         index: {
             entry: 'src/main.js',
             template: 'tpl/index.html',
-            title: 'Project Tracker'
+            title: BASE_PAGE_TITLE
         }
     },
     devServer: {
